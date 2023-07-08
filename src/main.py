9d9f44523ecc7f216b6d51a1dc8fa4d9bb03bea4
Matthew Nekirov <matthew.nekirov@gmail.com>
2023-07-08 11:58:13 +0300

    feat: add minmax widget

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import sys
+from pathlib import Path
 
+import pandas as pd
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QTabWidget
 
+from src.minmax_widget import MinMaxWidget
 from src.pt_scatter_widget import PTScatterWidget
 
 
@@ -14,13 +17,26 @@ class AboutPage(QWidget):
         self.setLayout(layout)
 
 
+def load_dataset(path: Path) -> pd.DataFrame:
+    df = pd.read_json(path)
+    df.date = pd.to_datetime(df.date, format="%Y-%m-%d %H:%M:%S")
+    df.set_index("date", inplace=True)
+    return df
+
+
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self._main = QTabWidget()
         self.setCentralWidget(self._main)
-        self._main.addTab(PTScatterWidget(), "PT Scatter")
-        for text in ("second", "third", "fourth", "fifth"):
+
+        df = load_dataset("../datasets/Hydra-L.json")
+
+        self._main.addTab(PTScatterWidget(df), "PT Scatter")
+        self._main.addTab(
+            MinMaxWidget("Temperature ($^\circ$C)", df.BME280_temp), "MinMax"
+        )
+        for text in ("third", "fourth", "fifth"):
             self._main.addTab(AboutPage(text), text.capitalize())
 
 
--- src/pt_scatter_widget.py
+++ src/pt_scatter_widget.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pandas as pd
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout
@@ -8,20 +6,12 @@ from matplotlib.figure import Figure
 import matplotlib.dates as mdates
 
 
-def load_dataset(path: Path) -> pd.DataFrame:
-    df = pd.read_json(path)
-    df.date = pd.to_datetime(df.date, format="%Y-%m-%d %H:%M:%S")
-    df.set_index("date", inplace=True)
-    return df
-
-
 class PTScatterWidget(QWidget):
-    def __init__(self):
+    def __init__(self, df: pd.DataFrame) -> None:
         super().__init__()
         self._layout = QVBoxLayout(self)
         self.setLayout(self._layout)
 
-        df = load_dataset("../datasets/Hydra-L.json")
         df_mean = df.resample("5min").mean()
 
         static_canvas = FigureCanvas(Figure())
